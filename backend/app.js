96b72e408de4d0c871cb2ed351247a3399d61a74
Kenneth Traynor <cyannotive@gmail.com>
2021-07-30 14:40:58 -0400

    fix: checking cors

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
+// const helmet = require('helmet');
+// const rateLimit = require('express-rate-limit');
 
 const { celebrate, Joi, errors } = require('celebrate');
 const auth = require('./middlewares/auth');
@@ -13,8 +13,8 @@ const { createUser } = require('./controllers/createUser');
 const { login } = require('./controllers/login');
 const userRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
-const { errorHandler } = require('./controllers/errorHandler');
-const NotFoundError = require('./errors/not-found-error');
+// const { errorHandler } = require('./controllers/errorHandler');
+// const NotFoundError = require('./errors/not-found-error');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -26,27 +26,22 @@ mongoose.connect('mongodb://localhost:27017/aroundb', {
   useFindAndModify: false,
 });
 
-const limiter = rateLimit({
-  windowMs: 60000,
-  max: 50,
-});
+// const limiter = rateLimit({
+//   windowMs: 60000,
+//   max: 50,
+// });
 
 app.use(cors());
 app.options('*', cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+// app.use(limiter);
 
-app.use(limiter);
-
-app.use(helmet());
+// app.use(helmet());
 
 app.use(requestLogger);
 
-app.use('/users', auth, userRouter);
-app.use('/cards', auth, cardsRouter);
+// app.use('/users', auth, userRouter);
+// app.use('/cards', auth, cardsRouter);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
@@ -68,12 +63,17 @@ app.post('/signin', celebrate({
   }),
 }), login);
 
-app.get('*', (req, res, next) => next(new NotFoundError('Requested resource not found')));
+// app.get('*', (req, res, next) => next(new NotFoundError('Requested resource not found')));
 
 app.use(errorLogger);
 
 app.use(errors());
 
-app.use(errorHandler);
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({ message: statusCode === 500 ? 'Internal Server Error' : message });
+});
 
 app.listen(PORT);
