bd5c28393c983a4f40568e6784ccf4a40107b76e
Kenneth Traynor <cyannotive@gmail.com>
2021-07-30 13:07:57 -0400

    fix: change logs output location

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
-// middlewares
 const { celebrate, Joi, errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-// Routes and Controllers
 const { createUser } = require('./controllers/createUser');
 const { login } = require('./controllers/login');
 const userRouter = require('./routes/users');
@@ -26,9 +26,17 @@ mongoose.connect('mongodb://localhost:27017/aroundb', {
   useFindAndModify: false,
 });
 
+const limiter = rateLimit({
+  windowMs: 60000,
+  max: 50,
+});
 
 app.use(cors());
 app.options('*', cors());
+
+app.use(limiter);
+
+app.use(helmet());
 
 app.use(requestLogger);
 
@@ -61,11 +69,6 @@ app.use(errorLogger);
 
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({ message: statusCode === 500 ? 'Internal Server Error' : message });
-});
+app.use(errorHandler);
 
 app.listen(PORT);
